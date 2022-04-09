9892b61167f01e09bea94745be0186aa813d7c9c
vnikonov63 <nikonov.va@yandex.ru>
2022-04-09 05:33:17 -0700

    0.0.1

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import { modelDriver } from "./models/driver.js";
+
+mongoose.connect(
+  "mongodb+srv://vas:1234@cluster0.qkgem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true}
+);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/getDrivers", async (req, res) => {
+  const drivers = await modelDriver.find();
+  res.json(drivers);
+});
+
+app.post("/createNewDriver", async (req, res) => {
+  const {firstName, lastName, phone} = req.body;
+  const small = new modelDriver({firstName: firstName, lastName:lastName, phone: phone, status: "waiting" });
+  small.save();
+  res.send(204)
+})
+
+app.patch("/status", async (req,res) => {
+  const userId = req.body.id;
+
+  const driver = await modelDriver.findById(userId);
+  console.log(driver);
+
+  const newStatus = req.body.status;
+
+  driver.status = newStatus;
+
+  driver.save()
+  
+  res.send(200)
+})
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`The server is app and listening on port ${port}`);
+});
